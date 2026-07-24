2c4865294df9a5f66f437ed18759ff7051d6b090
andreag-bit <sanromanandreag@gmail.com>
2026-07-24 19:12:13 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-base: '/ca-precision-carpentry/',
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/ca-precision-carpentry/',
